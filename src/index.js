da995293406b79b7df64d873e6f5c9a0a9ae12c6
Rdjarbeng <johndoe@example.com>
2021-07-07 08:29:27 +0000

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import * as serviceWorker from './serviceWorker'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
-  
-    <App />
-  
+    <App />,
   document.getElementById('root')
 );
 
